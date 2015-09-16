3c286cc6a15b2774baf898256c1e74ed827e280f
Richard Simpson <richardsimpson@outlook.com>
2015-09-16 11:57:51 -0500

    Added helper attribute for quickly getting up and running with the BrowserTestFramework.

---
+++ src/Browser.xUnit/BrowserTestFrameworkAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Xunit.Sdk;
+
+namespace Browser.xUnit
+{
+    [TestFrameworkDiscoverer("Browser.xUnit.Sdk.BrowserTestFrameworkTypeDiscoverer", "Browser.xUnit")]
+    [AttributeUsage(AttributeTargets.Assembly)]
+
+    public class BrowserTestFrameworkAttribute : Attribute, ITestFrameworkAttribute
+    {
+    }
+}
