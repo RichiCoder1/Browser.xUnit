ab63a8954c698a500ac4fb151b7894cdf4c1b97a
Richard Simpson <richardsimpson@outlook.com>
2015-09-16 18:07:51 -0500

    #4: Create Sample Project

---
+++ samples/ExampleTest/TestLifetimeExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Browser.xUnit;
+
+using ExampleTest;
+
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: BrowserTestFramework]
+[assembly: BeforeAfterAssemblyTests(typeof(TestLifetimeExample))]
+
+namespace ExampleTest
+{
+    public class TestLifetimeExample : IBeforeAfterAssemblyTests
+    {
+        private readonly IMessageSink _messageSink;
+
+        public TestLifetimeExample(IMessageSink messageSink)
+        {
+            _messageSink = messageSink;
+        }
+
+        /// <summary>
+        /// Executes before execution of an assemblies tests.
+        /// </summary>
+        public Task BeforeTestAssemblyStartedAsync()
+        {
+            _messageSink.OnMessage(new DiagnosticMessage("BeforeTestAssemblyStartedAsync"));
+            return Task.FromResult(true);
+        }
+
+        /// <summary>
+        /// Executes after execution of an assemblies tests.
+        /// </summary>
+        public Task AfterTestAssemblyFinishingAsync()
+        {
+            _messageSink.OnMessage(new DiagnosticMessage("AfterTestAssemblyFinishingAsync"));
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ samples/Readme.md
@@ -0,0 +1,5 @@
+# Samples
+
+Contains a set of example samples for using the Browser Test Framework in your project.
+
+For DNX pased sample projects, simply run `dnx test -diagnostics` to see the expected output of running tests.
