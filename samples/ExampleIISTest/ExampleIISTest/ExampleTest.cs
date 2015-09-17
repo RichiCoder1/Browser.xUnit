a5ea9f504dbaf69a0faa084eb4efc0215e8f8e61
Richard Simpson <richard.simpson@finalphasesystems.com>
2015-09-17 14:37:55 -0500

    Added an example test for spinning up a test running against a test server.

---
+++ samples/ExampleIISTest/ExampleIISTest/ExampleTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Browser.xUnit;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ExampleTest
+{
+    public class ExampleTest : INeedToKnowTestFailure
+    {
+        private string Url => TestLifetimeExample.Configuration["Tests:ExampleSite:Url"];
+
+        private HttpClient Client => new HttpClient {BaseAddress = new Uri(Url)};
+
+        [BrowserFact]
+        public async Task PassingTest()
+        {
+            var result = await Client.GetAsync("index.html");
+            result.EnsureSuccessStatusCode();
+        }
+
+        [BrowserFact]
+        public async Task FailingTest()
+        {
+            var result = await Client.GetAsync("index.json");
+            result.EnsureSuccessStatusCode();
+        }
+
+        #region Implementation of INeedToKnowTestFailure
+
+        public Task HandleFailureAsync(ITest test, Exception exception)
+        {
+            return Task.FromResult(true);
+        }
+
+        #endregion
+    }
+}
---
+++ samples/ExampleIISTest/ExampleIISTest/TestLifetimeExample.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Browser.xUnit;
+
+using ExampleTest;
+using Microsoft.Framework.Configuration;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: BrowserTestFramework]
+[assembly: BeforeAfterAssemblyTests(typeof(TestLifetimeExample))]
+
+namespace ExampleTest
+{
+    public class TestLifetimeExample : IBeforeAfterAssemblyTests
+    {
+        private readonly IMessageSink _messageSink;
+        private Process _webProcess;
+        public static readonly IConfigurationRoot Configuration;
+
+        static TestLifetimeExample()
+        {
+            Configuration = new ConfigurationBuilder(Assembly.GetExecutingAssembly().Location)
+                .AddJsonFile("config.json")
+                .Build();
+        }
+
+        public TestLifetimeExample(IMessageSink messageSink)
+        {
+            _messageSink = messageSink;
+        }
+
+        /// <summary>
+        /// Executes before execution of an assemblies tests.
+        /// </summary>
+        public Task BeforeTestAssemblyStartedAsync()
+        {
+            var args = GetProcessArguments();
+            _messageSink.OnMessage(new DiagnosticMessage($"Command: {args.Item1} {args.Item2}"));
+            _webProcess = Process.Start(args.Item1, args.Item2);
+            return Task.FromResult(true);
+        }
+
+        /// <summary>
+        /// Executes after execution of an assemblies tests.
+        /// </summary>
+        public Task AfterTestAssemblyFinishingAsync()
+        {
+            _messageSink.OnMessage(new DiagnosticMessage($"Cleaning up Kestrel"));
+            _webProcess.Kill();
+            _webProcess.Dispose();
+            return Task.FromResult(true);
+        }
+
+        public Tuple<string, string> GetProcessArguments()
+        {
+            var command = Configuration["Tests:ExampleSite:Command"];
+            var process = command.Substring(0, command.IndexOf(" ", StringComparison.Ordinal));
+            var arguments = command.Substring(command.IndexOf(" ", StringComparison.Ordinal));
+            return Tuple.Create(process, arguments);
+        } 
+    }
+}
